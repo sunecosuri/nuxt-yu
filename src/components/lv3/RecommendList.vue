04307e7c5af374e7127ea1d502d3807f9e0a6142
sunecosuri <sunecosuri@gmail.com>
2018-12-12 19:13:55 +0900

    辛いので一旦コミット

---
+++ src/components/lv3/RecommendList.vue
@@ -0,0 +1,91 @@
+<template>
+  <ul :class="[$style.grid, $style.cal3]">
+    <li :class="[$style.item]">
+      <v-card>
+        <v-img
+          src="~/assets/img/img-sample.png"
+          aspect-ratio="2.75"
+        ></v-img>
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+            <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
+          </div>
+        </v-card-title>
+
+        <v-card-actions>
+          <v-btn flat color="orange">Share</v-btn>
+          <v-btn flat color="orange">Explore</v-btn>
+        </v-card-actions>
+      </v-card>
+    </li>
+  </ul>
+</template>
+
+<style module>
+@import '../../assets/css/values.css';
+
+.grid {
+  display: flex;
+  flex-wrap: wrap;
+}
+.item {
+  padding-bottom: var(--xwide);
+}
+.grid.cal3 > .item {
+  width: calc(100% / 3);
+  padding: var(--narrow);
+}
+.title {
+  width: 100%;
+  margin-bottom: var(--narrow);
+  padding: 0 var(--default);
+  overflow: hidden;
+  font-size: 1.2rem;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+@media screen and (min-width: 600px) {
+  .title {
+    padding: 0;
+  }
+}
+.card {
+  position: relative;
+  width: 100%;
+}
+.img {
+  width: 100%;
+  height: 90px;
+  object-fit: cover;
+}
+@media screen and (min-width: 600px) {
+  .img {
+    height: 160px;
+  }
+}
+.overText {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 40%;
+  padding: calc(var(--wide) * 1.4) var(--default) var(--narrow);
+  background-image: linear-gradient(-180deg, rgba(255,255,255,0.00) 0%, rgba(255,255,255,0.80) 50%);
+}
+@media screen and (min-width: 600px) {
+  .overText {
+    padding: var(--wide) var(--narrow) var(--narrow);
+  }
+}
+.text {
+  display: block;
+  width: 100%;
+  height: 20px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  color: #4F4F4F;
+  font-size: 1rem;
+}
+
+</style>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,106 +1,60 @@
 <template>
-  <v-app>
-    <v-navigation-drawer
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile
-          v-for="(item, i) in items"
-          :to="item.to"
-          :key="i"
-          router
-          exact
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon" />
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title" />
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-toolbar-side-icon @click="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'" />
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"/>
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
-      </v-btn>
-    </v-toolbar>
-    <v-content>
-      <v-container>
+  <section :class="$style.nav">
+    <global-nav />
+    <article :class="$style.article">
+      <header :class="$style.header">
+        <global-header />
+      </header>
+      <div :class="$style.contents">
         <nuxt />
-      </v-container>
-    </v-content>
-    <v-navigation-drawer
-      :right="right"
-      v-model="rightDrawer"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :fixed="fixed"
-      app
-    >
-      <span>&copy; 2017</span>
-    </v-footer>
-  </v-app>
+      </div>
+      <global-footer />
+    </article>
+  </section>
 </template>
 
 <script>
+  import GlobalHeader from '~/components/TheGlobalHeader.vue'
+  import GlobalFooter from '~/components/TheGlobalFooter.vue'
+  import GlobalNav from '~/components/TheGlobalNav.vue'
+
   export default {
-    data() {
-      return {
-        clipped: false,
-        drawer: true,
-        fixed: false,
-        items: [
-          { icon: 'apps', title: 'Welcome', to: '/' },
-          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
-        ],
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'Vuetify.js'
-      }
+    components: {
+      GlobalHeader,
+      GlobalFooter,
+      GlobalNav
     }
   }
 </script>
+
+<style module>
+@import '../assets/css/values.css';
+
+.article {
+  min-height: 100vh;
+  background: var(--gray-light);
+  font-family: -apple-system, BlinkMacSystemFont, "Helvetica Neue", YuGothic, "ヒラギノ角ゴ ProN W3", Hiragino Kaku Gothic ProN, Arial, "メイリオ", Meiryo, sans-serif;
+}
+.contents {
+  min-height: calc(100vh - 48px);
+  color: var(--base-font-color);
+}
+.nav {
+  display: flex;
+  position: sticky;
+  top: 0;
+}
+.header {
+  position: sticky;
+  top: 0;
+  display: block;
+  width: 100%;
+  height: 48px;
+  z-index: 9999;
+}
+.article {
+  width: 100%;
+  min-height: 100vh;
+  margin: 0;
+}
+</style>
